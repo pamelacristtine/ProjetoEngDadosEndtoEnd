d6b05e7c9309fa8959955fb254122785f1950e1a
Pâmela Cristine Amorim Joanas <114619250+pamelacristtine@users.noreply.github.com>
2024-11-05 14:23:20 -0300

    Update create_tabela.sql

--- sql/create_tabela.sql
+++ sql/create_tabela.sql
@@ -1,2 +1,17 @@
-
-CREATE TABLE [dbo].[Violencia_Domestica_MG] (    [DT_NOTIFIC] date NULL,    [DT_NASC] date NULL,    [IDADE] int NULL,    [RACA] varchar(15) NULL,    [MUNICIPIO] varchar(35) NULL,    [LOCAL_OCOR] varchar(30) NULL,    [OUT_VEZES] varchar(10) NULL,    [LES_AUTOP] varchar(10) NULL,    [VIOL_FISIC] varchar(10) NULL,    [VIOL_PSICO] varchar(10) NULL,    [VIOL_SEXU] varchar(10) NULL,    [NUM_ENVOLV] varchar(20) NULL,    [AUTOR_SEXO] varchar(15) NULL,    [ORIENT_SEX] varchar(15) NULL,    [IDENT_GEN] varchar(20) NULL);
+CREATE TABLE [dbo].[Violencia_Domestica_MG] (
+[DT_NOTIFIC] date NULL,
+[DT_NASC] date NULL,
+[IDADE] int NULL,
+[RACA] varchar(15) NULL,
+[MUNICIPIO] varchar(35) NULL,
+[LOCAL_OCOR] varchar(30) NULL,
+[OUT_VEZES] varchar(10) NULL,
+[LES_AUTOP] varchar(10) NULL,
+[VIOL_FISIC] varchar(10) NULL,
+[VIOL_PSICO] varchar(10) NULL,
+[VIOL_SEXU] varchar(10) NULL,
+[NUM_ENVOLV] varchar(20) NULL,
+[AUTOR_SEXO] varchar(15) NULL,
+[ORIENT_SEX] varchar(15) NULL,
+[IDENT_GEN] varchar(20) NULL
+);
